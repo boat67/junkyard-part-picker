a032d277b402bdcbec8858aec727b96b6facc821
boat67 <bepo2002@gmail.com>
2026-09-10 11:19:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ if submit:
                 """
 
                 response = client.models.generate_content(
-                    model="gemini-3.6-flash",
+                    model="gemini-2.5-flash",
                     contents=prompt
                 )
 
